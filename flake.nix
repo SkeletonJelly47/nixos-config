c7b5f07de3c2141366581f624060d2f8f97e8892
skeletonjelly47 <mikkelsson4hotmail.com>
2025-05-25 17:53:43 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let 
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+    };
+
+    homeConfigurations = {
+      mikko5 = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
