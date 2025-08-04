649e8b48e1c9ff034fd8e4f0edc170e104a41725
SkeletonJelly47 <mikkelsson4hotmail.com>
2025-08-04 19:50:47 +0300

    nvidia beta

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,7 @@
       modesetting.enable = true;
       powerManagement.enable = false;
       powerManagement.finegrained = false;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
 
     bluetooth.enable = true;
