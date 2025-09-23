c1083ab9bcb75804606ba59c8219e1e1810c66f7
SkeletonJelly47 <mikkelsson4hotmail.com>
2025-09-23 17:46:27 +0300

    Explicitly enable firewall, add enable fail2ban

--- configuration.nix
+++ configuration.nix
@@ -227,11 +227,26 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  networking.firewall.enable = true;
+
+  services.fail2ban = {
+    enable = true;
+
+    maxretry = 2;
+    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime-increment = {
+      enable = true; # Enable increment of bantime after each violation
+      #formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # Do not ban for more than 1 week
+      overalljails = true; # Calculate the bantime based on all the violations
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
