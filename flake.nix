f237f928842e315b45902a2e20f6d51c41f79d6e
SkeletonJelly47 <mikkelsson4hotmail.com>
2025-12-01 20:14:08 +0200

    Update nixpkgs and home-manager to 25.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # NixOS official package source, using the nixos-25.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
