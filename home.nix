39b8d1e07d2250186cd634ac6672c494b90374bd
SkeletonJelly47 <mikkelsson4hotmail.com>
2025-12-14 21:14:35 +0200

    Add orca-slicer env args

--- home.nix
+++ home.nix
@@ -29,6 +29,7 @@
     archipelago
     r2modman
 
+    # env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1
     unstable.orca-slicer
   ];
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
