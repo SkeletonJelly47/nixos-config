f12e34fb889389a825d2437cb324a6ef88ea876f
SkeletonJelly47 <mikkelsson4hotmail.com>
2026-01-08 12:49:17 +0200

    Add owmods-gui

--- home.nix
+++ home.nix
@@ -32,6 +32,7 @@
     archipelago
     poptracker
     r2modman
+    owmods-gui # Launch with WEBKIT_DISABLE_COMPOSITING_MODE=1 https://github.com/ow-mods/ow-mod-man/issues/899#issuecomment-2608313165
 
     itch
 
