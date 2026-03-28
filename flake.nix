c6765b68a4f86e9759fe732464349140939964a6
SkeletonJelly47 <mikkelsson4hotmail.com>
2026-03-28 20:33:26 +0200

    parting my it... and by it... i mean my flake

--- flake.nix
+++ flake.nix
@@ -6,56 +6,60 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     yeetmouse = {
       url = "github:AndyFilter/YeetMouse?dir=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
+    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    yeetmouse,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations.nixos = lib.nixosSystem {
-      inherit system;
-      specialArgs = {inherit inputs outputs;};
-      modules = [
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              unstable = import nixpkgs-unstable {
-                inherit prev;
-                system = prev.system;
-                config.allowUnfree = true;
-              };
-            })
-          ];
-        }
-        ./configuration.nix
+  outputs = inputs: inputs.flake-parts.lib.mkFlake
+  { inherit inputs; }
+  (inputs.import-tree ./modules);
 
-        yeetmouse.nixosModules.default
+  # outputs = {
+  #   self,
+  #   nixpkgs,
+  #   nixpkgs-unstable,
+  #   home-manager,
+  #   yeetmouse,
+  #   ...
+  # } @ inputs: let
+  #   inherit (self) outputs;
+  #   lib = nixpkgs.lib;
+  #   system = "x86_64-linux";
+  # in {
+  #   nixosConfigurations.nixos = lib.nixosSystem {
+  #     inherit system;
+  #     specialArgs = {inherit inputs outputs;};
+  #     modules = [
+  #       {
+  #         nixpkgs.overlays = [
+  #           (final: prev: {
+  #             unstable = import nixpkgs-unstable {
+  #               inherit prev;
+  #               system = prev.system;
+  #               config.allowUnfree = true;
+  #             };
+  #           })
+  #         ];
+  #       }
+  #       ./configuration.nix
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+  #       yeetmouse.nixosModules.default
 
-          home-manager.users.mikko5 = import ./home.nix;
-        }
-      ];
-    };
-  };
+  #       home-manager.nixosModules.home-manager
+  #       {
+  #         home-manager.useGlobalPkgs = true;
+  #         home-manager.useUserPackages = true;
+
+  #         home-manager.users.mikko5 = import ./home.nix;
+  #       }
+  #     ];
+  #   };
+  # };
 }
---
+++ modules/hosts/mikko5/default.nix
@@ -0,0 +1,18 @@
+{ self, inputs, ... }: {
+  flake.nixosConfigurations.mikko5 = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.mikko5Configuration
+      {
+        nixpkgs.overlays = [
+          (final: prev: {
+            unstable = import inputs.nixpkgs-unstable {
+              inherit prev;
+              system = prev.system;
+              config.allowUnfree = true;
+            };
+          })
+        ];
+      }
+    ];
+  };
+}
