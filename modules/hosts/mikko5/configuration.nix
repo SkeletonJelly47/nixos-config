df561004f69b192b07895764fe678365eabe59d3
SkeletonJelly47 <mikkelsson4hotmail.com>
2026-04-04 23:34:10 +0300

    Orca slicer notes

--- modules/hosts/mikko5/configuration.nix
+++ modules/hosts/mikko5/configuration.nix
@@ -249,7 +249,19 @@
       itch
 
       # env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1
-      unstable.orca-slicer
+      # https://github.com/NixOS/nixpkgs/issues/345590#issuecomment-3904433594
+      orca-slicer
+      # (pkgs.orca-slicer.overrideAttrs (prev: {
+      # preFixup = (prev.preFixup or "") + ''
+      #   gappsWrapperArgs+=(
+      #     --set __GLX_VENDOR_LIBRARY_NAME mesa
+      #     --set __EGL_VENDOR_LIBRARY_FILENAMES /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json
+      #     --set MESA_LOADER_DRIVER_OVERRIDE zink
+      #     --set GALLIUM_DRIVER zink
+      #     --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+      #   )
+      # '';
+      # }))
 
       jetbrains.rust-rover
 
