254450858853e6b0f1e7fb7fd6ec0c66f639ebac
SkeletonJelly47 <mikkelsson4hotmail.com>
2026-06-21 15:36:33 +0300

    separate locale module

--- modules/hosts/mikko5/default.nix
+++ modules/hosts/mikko5/default.nix
@@ -3,9 +3,11 @@
   flake.nixosConfigurations.mikko5 = inputs.nixpkgs.lib.nixosSystem {
     modules =  with self.nixosModules; [
       mikkone5
+      locale
       shellAlias
       yeetmouse
       fail2ban
+
       bluetooth
       audio
     ];
